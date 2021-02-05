43e910760ce36209dabff22a0d801ac4fda11308
Emmanuelle Laguna <emmanuelle.laguna@gmail.com>
2021-02-05 08:25:43 -0600

    Tipos de Datos

---
+++ tiposDeDatos/tiposDeDatos.py
@@ -0,0 +1,10 @@
+# strings
+palabra = 'hola mundo'
+oracion = "hola mundo con comilla doble"
+
+#numericos
+entero = 20
+flotante = 20.2
+complejos = 1j
+
+print(palabra, oracion, entero, flotante, complejos)
