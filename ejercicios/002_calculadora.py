82d65822b1ecfc635f9986bae01436f69110379f
Emmanuelle Laguna <emmanuelle.laguna@gmail.com>
2021-02-20 12:26:14 -0600

    Crando opciones de operaciones en calculadora

--- ejercicios/002_calculadora.py
+++ ejercicios/002_calculadora.py
@@ -5,11 +5,23 @@ primerNumero = input("Ingrese el primer número ")
 if primerNumero == "t" or primerNumero == "T":
     exit()
 
+simbolo = input("Ingrese operación ")
+
 segundoNumero = input("Ingrese el segundo número ")
 
 try:
     primerNumero = int(primerNumero)
     segundoNumero = int(segundoNumero)
-    print(primerNumero + segundoNumero)
+    if simbolo == "+":
+        print(primerNumero + segundoNumero)
+    elif simbolo == "-":
+        print(primerNumero - segundoNumero)
+    elif simbolo == "*":
+        print(primerNumero * segundoNumero)
+    elif simbolo == "/":
+        print(primerNumero / segundoNumero)
+    else:
+        print("El simbolo ingresado no es valido")
 except:
     print("Alguno de los dos números no es un número")
+    exit()
