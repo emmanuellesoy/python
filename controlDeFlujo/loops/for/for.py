83a499042240d3512bcb4e0ce664d67bb7bf3f60
Emmanuelle Laguna <emmanuelle.laguna@gmail.com>
2021-02-20 12:51:51 -0600

    Loops

---
+++ controlDeFlujo/loops/for/for.py
@@ -0,0 +1,18 @@
+usuarios = ["Juan", "María", "Emmanuelle", "Pedro"]
+
+for usuario in usuarios:
+    print(usuario)
+
+for x in range(5, 30, 3):
+    print(x)
+else:
+    print("Fin de lista")
+
+
+edades = [25, 35, 23, 36]
+
+i = 0
+
+for usuario in usuarios:
+    print(usuario, edades[i])
+    i = + 1
