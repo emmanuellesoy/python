800ef0eb45f025dd62fdfe7a54bc52c5db895d8c
Emmanuelle Laguna <emmanuelle.laguna@gmail.com>
2021-03-15 13:25:47 -0600

    Leer archivos por linea

--- archivos/archivos.py
+++ archivos/archivos.py
@@ -1,2 +1,14 @@
 datos = open("datos.txt")
-print(datos.read())
+# print(datos.read())
+
+# print(datos.readline())
+# print(datos.readline())
+# print(datos.readline())
+# print(datos.readline())
+# print(datos.readline())
+
+
+for dato in datos:
+    print(dato)
+
+datos.close()
